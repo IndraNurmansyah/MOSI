01d2f506b0296fde02abdcf648866f181715df66
handydannu <handydannu@hotmail.com>
2014-06-29 12:45:55 +0700

    Ganti Project

---
+++ includes/hitung.php
@@ -0,0 +1,93 @@
+<?php
+
+    //inisialisasi X1 dari input
+    $x1 = $_POST['x1'];
+    $x2 = $_POST['x2'];
+    $x3 = $_POST['x3'];
+    $x4 = $_POST['x4'];
+    $x5 = $_POST['x5'];
+    $x6 = $_POST['x6'];
+    $x7 = $_POST['x7'];
+    $x8 = $_POST['x8'];
+    $x9 = $_POST['x9'];
+    $x10 = $_POST['x10'];
+    $x11 = $_POST['x11'];
+    $x12 = $_POST['x12'];
+    $x13 = $_POST['x13'];
+    $x14 = $_POST['x14'];
+    $x15 = $_POST['x15'];
+    $x16 = $_POST['x16'];
+    $x17 = $_POST['x17'];
+    $x18 = $_POST['x18'];
+    $x19 = $_POST['x19'];
+    $x20 = $_POST['x20'];
+    //inisialisasi Y1 dari input
+    $y1 = $_POST['y1'];
+    $y2 = $_POST['y2'];
+    $y3 = $_POST['y3'];
+    $y4 = $_POST['y4'];
+    $y5 = $_POST['y5'];
+    $y6 = $_POST['y6'];
+    $y7 = $_POST['y7'];
+    $y8 = $_POST['y8'];
+    $y9 = $_POST['y9'];
+    $y10 = $_POST['y10'];
+    $y11 = $_POST['y11'];
+    $y12 = $_POST['y12'];
+    $y13 = $_POST['y13'];
+    $y14 = $_POST['y14'];
+    $y15 = $_POST['y15'];
+    $y16 = $_POST['y16'];
+    $y17 = $_POST['y17'];
+    $y18 = $_POST['y18'];
+    $y19 = $_POST['y19'];
+    $y20 = $_POST['y20'];
+
+    //inisialisasi 1/Yi
+    $pery1 = round(@(1/$y1),5);
+    $pery2 = round(@(1/$y2),5);
+    $pery3 = round(@(1/$y3),5);
+    $pery4 = round(@(1/$y4),5);
+    $pery5 = round(@(1/$y5),5);
+    $pery6 = round(@(1/$y6),5);
+    $pery7 = round(@(1/$y7),5);
+    $pery8 = round(@(1/$y8),5);
+    $pery9 = round(@(1/$y9),5);
+    $pery10 = round(@(1/$y10),5);
+    $pery11 = round(@(1/$y11),5);
+    $pery12 = round(@(1/$y12),5);
+    $pery13 = round(@(1/$y13),5);
+    $pery14 = round(@(1/$y14),5);
+    $pery15 = round(@(1/$y15),5);
+    $pery16 = round(@(1/$y16),5);
+    $pery17 = round(@(1/$y17),5);
+    $pery18 = round(@(1/$y18),5);
+    $pery19 = round(@(1/$y19),5);
+    $pery20 = round(@(1/$y20),5);
+
+    //inisialisasi X kuadrat
+    $xx1 = $x1*$x1;
+    $xx2 = $x2*$x2;
+    $xx3 = $x3*$x3;
+    $xx4 = $x4*$x4;
+    $xx5 = $x5*$x5;
+    $xx6 = $x6*$x6;
+    $xx7 = $x7*$x7;
+    $xx8 = $x8*$x8;
+    $xx9 = $x9*$x9;
+    $xx10 = $x10*$x10;
+    $xx11 = $x11*$x11;
+    $xx12 = $x12*$x12;
+    $xx13 = $x13*$x13;
+    $xx14 = $x14*$x14;
+    $xx15 = $x15*$x15;
+    $xx16 = $x16*$x16;
+    $xx17 = $x17*$x17;
+    $xx18 = $x18*$x18;
+    $xx19 = $x19*$x19;
+    $xx20 = $x20*$x20;
+
+    //inisialisasi Xi/Yi
+	$xy1 = round(($x1 / $y1),5);
+
+?>
---
+++ tabelhitung.php
@@ -0,0 +1,177 @@
+<?php
+   include 'includes/hitung.php'; 
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <?php include 'includes/head.php'; ?>     	
+    </head> 
+<body> 
+<div id="mosi_header_wrapper">
+	<div id="mosi_header">
+    	<div id="site_title">PROGRAM PEMODELAN SIMULASI</div>
+        <p id="intro_text">PROGRAM PEMODELAN SIMULASI. Untuk menghitung soal resiprok</p>
+    </div>
+</div>
+<div id="mosi_main_wrapper">
+	<div id="mosi_main">
+		<div id="content"> 
+        <center>
+            <div id="beranda" class="section">
+                <h4 align="center">Hasil hitung</h4>
+                    <p align="center">Tabel hasil perhitungan (nilai x dan y)</p>
+                    <div id="contact_form">
+                        <center>
+                                <table id="box-table-c" align="center">
+                                  <tr>
+                                    <th>Nomor</th>
+                                    <th>Xi</th>
+                                    <th>Yi</th>
+                                    <th>(1/y)</th>
+                                    <th>(x^2)</th>
+                                    <th>(Xi/Yi)</th>
+                                  </tr>
+                                  <tr>
+                                    <td>1</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $pery1; ?></td>
+                                    <td><?php echo $xx1; ?></td>
+                                    <td><?php echo $xy1; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>2</td>
+                                    <td><?php echo $x2; ?></td>
+                                    <td><?php echo $y2; ?></td>
+                                    <td><?php echo $pery2; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>3</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>4</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>5</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>6</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>7</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>8</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>9</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>10</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>11</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>12</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>13</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>14</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>15</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>16</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  <tr>
+                                    <td>17</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>18</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>19</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                  <tr>
+                                    <td>20</td>
+                                    <td><?php echo $x1; ?></td>
+                                    <td><?php echo $y1; ?></td>
+                                    <td><?php echo $xy; ?></td>
+                                  </tr>
+                                </table>
+                            </form>
+                        </center>
+
+                <a href="index.php" class="home_btn">BERANDA</a> 
+                </div>
+        </center>   
+
+            </div>        
+            
+        </div> 
+    </div>
+</div>
+
+<div id="mosi_footer_wrapper">
+	<div id="mosi_footer">
+    	<p>Copyright © 2014 | <a href="http://moccadev.com">Handy D - MoccaDev</a> | Program Pemodelan Simulasi</p>
+    </div>
+</div>
+
+</body> 
+</html>
